da31db6a9403b952eb450912b99f385b58d91f0c
vlad <1250122403@qq.com>
2025-05-12 18:06:02 +0800

    苍穹外卖day07-缓存套餐业务功能

--- sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -25,6 +25,15 @@ public interface SetmealDishMapper {
     List<SetmealDish> getBySetmealId(Long setmealId);
 
     /**
+     * 根据菜品id查询对应的套餐id
+     * @param dishIds
+     * @return
+     */
+    //select setmeal_id from setmeal_dish where dish.id in (1, 2, 3, 4)
+    List<Long> getSetmealIdsByDishIds(List<Long> dishIds);
+
+
+    /**
      * 根据套餐id删除套餐和菜品的关联关系
      * @param setmealId
      */
--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,18 +1,47 @@
 package com.sky.service;
 
+import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
+import com.sky.result.PageResult;
 import com.sky.vo.DishVO;
 
 import java.util.List;
 
+
 public interface DishService {
+
     /**
-     * 根据分类id查询菜品
-     * @param categoryId
+     * 新增菜品和对应的口味数据
+     * @param dishDTO
+     */
+    public void saveWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
      * @return
      */
-    List<Dish> list(Long categoryId);
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 
+    /**
+     * 批量删除菜品
+     * @param ids
+     */
+    void deleteBatch(List<Long> ids);
+
+    /**
+     * 根据id查询菜品和对应的口味数据
+     * @param id
+     * @return
+     */
+    DishVO getByIdWithFlavor(Long id);
+
+    /**
+     * 根据id修改菜品基本信息和口味信息
+     * @param dishDTO
+     */
+    void updateWithFlavor(DishDTO dishDTO);
 
     /**
      * 条件查询菜品和口味
@@ -20,4 +49,19 @@ public interface DishService {
      * @return
      */
     List<DishVO> listWithFlavor(Dish dish);
+
+    /**
+     * 根据分类id查询菜品
+     * @param categoryId
+     * @return
+     */
+    List<Dish> list(Long categoryId);
+
+    /**
+     * 菜品起售停售
+     *
+     * @param status
+     * @param id
+     */
+    void startOrStop(Integer status, Long id);
 }
