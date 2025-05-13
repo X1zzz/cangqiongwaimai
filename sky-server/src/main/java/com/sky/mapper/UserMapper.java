0bedf58b9d3e0e434870241c1ce7df90780a70be
vlad <1250122403@qq.com>
2025-05-14 00:32:29 +0800

    苍穹外卖day08-订单支付finished

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -20,6 +20,9 @@ public interface UserMapper {
      * @param user
      */
     void insert(User user);
+
+    @Select("select * from user where id = #{userId}")
+    User getById(Long userId);
 }
 
 
